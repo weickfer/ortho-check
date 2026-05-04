966731ce247f9ba98ff60bafcc69b74ff759705f
Weickmam Ferreira <weickmam07@gmail.com>
2026-05-04 15:45:53 -0300

    fix crop

--- python/app/geoprocessing/raster_service.py
+++ python/app/geoprocessing/raster_service.py
@@ -3,8 +3,13 @@ from rasterio.mask import mask
 import geopandas as gpd
 import uuid
 
-def crop_tiff(tiff_path, geojson, output_dir="/data/outputs"):
-    gdf = gpd.GeoDataFrame.from_features(geojson["features"])
+def crop_tiff(tiff_path, geometry, output_dir="/data/outputs"):
+    # Verifica se recebemos um GeoJSON (dict) ou um objeto Shapely direto
+    if isinstance(geometry, dict):
+        gdf = gpd.GeoDataFrame.from_features(geometry["features"])
+    else:
+        # Se for um objeto shapely direto (como o 'geom'), cria o GeoDataFrame com ele
+        gdf = gpd.GeoDataFrame({'geometry': [geometry]})
 
     with rasterio.open(tiff_path) as src:
         gdf = gdf.set_crs("EPSG:4326").to_crs(src.crs)
