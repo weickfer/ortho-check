6f0d1db441df459cb1cb29ef6b645271d69650bb
Weickmam Ferreira <weickmam07@gmail.com>
2026-05-04 15:41:56 -0300

    crop file

--- python/app/services/audit_area_service.py
+++ python/app/services/audit_area_service.py
@@ -1,6 +1,7 @@
 from shapely import wkt
 from app.entities.audit_area import AuditArea
 from app.repositories.audit_area_repository import AuditAreaRepository
+from app.geoprocessing.raster_service import crop_tiff
 import math
 
 class AuditAreaService:
@@ -19,6 +20,9 @@ class AuditAreaService:
             captured_at=request.captured_at,
             area_sq_meters=area
         )
+
+        crop_tiff("/data/02-02-2026.tif", geom, "/data/outputs")
+
 
         return self.repo.save(db, entity)
 
